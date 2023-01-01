bd37d560f2fd70beace8e8ec440bb0ebcfb9abc3
GitHub Action <action@github.com>
2023-01-01 08:14:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/linwancen/synccoverage/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/linwancen/synccoverage/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.linwancen.synccoverage.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linwancen.synccoverage.MyBundle
 
 class MyApplicationService {
 
